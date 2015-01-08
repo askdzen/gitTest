be42bea3400a1aedc985783ee7ed04639d3d158c
Admin <ask.dzen@mail.ru>
2015-01-08 09:34:08 +0600

    branch commit

---
+++ src/Hello.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Admin on 1/8/2015.
+ */
+public class Hello {
+    public String message;
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage(){
+        return this.message;
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello my Big World!");
+        Hello custMessage=new Hello();
+        custMessage.setMessage("Hello my brother!");
+        System.out.println(custMessage.getMessage());
+
     }
 }
